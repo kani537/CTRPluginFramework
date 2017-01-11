e11f0b00e3912519b6d3eb0520f4bf4958a31a51
Nanquitas <nath.doidi@gmail.com>
2017-01-11 19:34:08 +0100

    Update Screen

--- Includes/CTRPluginFramework/Screen.hpp
+++ Includes/CTRPluginFramework/Screen.hpp
@@ -2,21 +2,20 @@
 #define CTRPLUGINFRAMEWORK_SCREEN_HPP
 
 #include "CTRPluginFramework.hpp"
+#include "ctrulib/services/gspgpu.h"
 
 namespace CTRPluginFramework
 {
-    struct  LCDFamebufferSetup
+    enum LCDSetup
     {
-        u16     width;
-        u16     height;
-        u32     leftScreen1;
-        u32     leftScreen2;
-        u32     format;
-        u32     index;
-        u32     disco;
-        u32     stride;
-        u32     rightScreen1;
-        u32     rightScreen2;
+        WidthHeight = 0x5C,     ///< Framebuffer width & height  Lower 16 bits: width, upper 16 bits: height
+        FramebufferA1 = 0x68,   ///< Framebuffer A first address For top screen, this is the left eye 3D framebuffer.
+        FramebufferA2 = 0x6C,   ///< Framebuffer A second address    For top screen, this is the left eye 3D framebuffer.
+        Format = 0x70,          ///< Framebuffer format  Bit0-15: framebuffer format, bit16-31: unknown
+        Select = 0x78,          ///< Framebuffer select  Bit0: which framebuffer to display, bit1-7: unknown
+        Stride = 0x90,          ///< Framebuffer stride  Distance in bytes between the start of two framebuffer rows (must be a multiple of 8).
+        FramebufferB1 = 0x94,   ///< Framebuffer B first address For top screen, this is the right eye 3D framebuffer. Unused for bottom screen.
+        FramebufferB2 = 0x98    ///< Framebuffer B second address    For top screen, this is the right eye 3D framebuffer. Unused for bottom screen.
     };
 
     class Screen
@@ -28,14 +27,32 @@ namespace CTRPluginFramework
 
         Screen(u32 lcdSetupInfo, u32 fillColorAddress);
 
-        static void    Initialize(void);
-        void    Flash(Color &color);
+        static void     Initialize(void);
+
+        void                        Flash(Color &color);
+        void                        SwapBuffer(void);
+        GSPGPU_FramebufferFormats   GetFormat(void);
+        u16                         GetWidth(void);
+        u16                         GetHeight(void);
+        u32                         GetStride(void);
+
+        u8                          *GetLeftFramebuffer(bool current = false);
+        u8                          *GetRightFramebuffer(bool current = false);                    
+        
 
 
     private:
-        LCDFamebufferSetup  *_LCDSetup;
-        u32                 *const _FillColor;
-        bool                _isInit;
+        u32                         *_LCDSetup;
+        u32                         *_FillColor;
+        u8                          *_leftFramebuffers[2];
+        u8                          *_rightFramebuffers[2];
+        u32                         *_currentBuffer;
+        
+        u16                         _width;
+        u16                         _height;
+        u32                         _stride;
+        bool                        _isInit;
+        GSPGPU_FramebufferFormats   _format;
     };
 }
 
--- Sources/CTRPluginFramework/Screen.cpp
+++ Sources/CTRPluginFramework/Screen.cpp
@@ -10,11 +10,59 @@ namespace CTRPluginFramework
     Screen *Screen::Top = NULL;
     Screen  *Screen::Bottom = NULL;
 
+    u32     FromPhysicalToVirtual(u32 address)
+    {
+        if (address >= 0x18000000 && address <= 0x20000000)
+            address += 0x07000000;
+        else if (address >= 0x20000000 && address <= 0x30000000)
+        {
+            s64     value = 0;
+            svcGetProcessInfo(&value, 0xFFFF8001, 20);
+            s32     offset = (s32)(value & 0xFFFFFFFF);
+            if (value == 0)
+                return (value);
+            else if (offset < 0x10000000)
+                address -= offset;
+            else
+                address += offset;
+        }
+        else
+            return (0);
+        return (address);
+    }
+
     Screen::Screen(u32 lcdSetupInfo, u32 fillColorAddress) : 
-    _LCDSetup(reinterpret_cast<LCDFamebufferSetup *>(lcdSetupInfo)),
+    _LCDSetup((u32 *)(lcdSetupInfo)),
     _FillColor((u32 *)fillColorAddress),
     _isInit(true)
     {
+        // Get format
+        _format = (GSPGPU_FramebufferFormats)(*(u32 *)(_LCDSetup + LCDSetup::Format) & 0xFFFF);
+
+        // Get width & height
+        u32 wh = *(u32 *)(_LCDSetup + LCDSetup::WidthHeight);
+        _width = (u16)(wh & 0xFFFF);
+        _height = (u16)(wh >> 16);
+
+        // Get left framebuffers pointers (Physical need to be converted)
+        _leftFramebuffers[0] = (u8 *)(*(u32 *)(_LCDSetup + LCDSetup::FramebufferA1));
+        _leftFramebuffers[1] = (u8 *)(*(u32 *)(_LCDSetup + LCDSetup::FramebufferA2));
+
+        // Get right framebuffers pointers (Physical need to be converted)
+        _rightFramebuffers[0] = (u8 *)(*(u32 *)(_LCDSetup + LCDSetup::FramebufferB1));
+        _rightFramebuffers[1] = (u8 *)(*(u32 *)(_LCDSetup + LCDSetup::FramebufferB2));
+
+        // Set current buffer pointers
+        _currentBuffer = (u32 *)(_LCDSetup + LCDSetup::Select);
+
+        // Get stride
+        _stride = *(u32 *)(_LCDSetup + LCDSetup::Stride);
+
+        // Converting the framebuffers
+        _leftFramebuffers[0] = (u8 *)FromPhysicalToVirtual((u32)_leftFramebuffers[0]);
+        _leftFramebuffers[1] = (u8 *)FromPhysicalToVirtual((u32)_leftFramebuffers[1]);
+        _rightFramebuffers[0] = (u8 *)FromPhysicalToVirtual((u32)_rightFramebuffers[0]);
+        _rightFramebuffers[1] = (u8 *)FromPhysicalToVirtual((u32)_rightFramebuffers[1]);
 
     }
 
@@ -36,5 +84,52 @@ namespace CTRPluginFramework
             svcSleepThread(5000000); // 0.005 second
         }
         *(_FillColor) = 0;
+    }
+
+    void    Screen::SwapBuffer(void)
+    {
+        *_currentBuffer = !*_currentBuffer;
+    }
+
+    GSPGPU_FramebufferFormats   Screen::GetFormat(void)
+    {
+        return (_format);
+    }
+
+    u16     Screen::GetWidth(void)
+    {
+        return (_width);
+    }
+
+    u16     Screen::GetHeight(void)
+    {
+        return (_height);
+    }
+
+    u32     Screen::GetStride(void)
+    {
+        return (_stride);
+    }
+
+    u8      *Screen::GetLeftFramebuffer(bool current)
+    {
+        u32    index = *_currentBuffer & 0b1;
+
+        if (current)
+        {
+            return (_leftFramebuffers[index]); 
+        }
+        return (_leftFramebuffers[!index]);            
+    }
+
+    u8      *Screen::GetRightFramebuffer(bool current)
+    {
+        u32    index = *_currentBuffer & 0b1;
+
+        if (current)
+        {
+            return (_rightFramebuffers[index]); 
+        }
+        return (_rightFramebuffers[!index]);            
     }
 }
