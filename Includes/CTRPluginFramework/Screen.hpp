fee7028f9c8c384bece87462658972238e061356
Nanquitas <nath.doidi@gmail.com>
2017-01-13 00:12:12 +0100

    Change enum scope

--- Includes/CTRPluginFramework/Screen.hpp
+++ Includes/CTRPluginFramework/Screen.hpp
@@ -6,21 +6,21 @@
 
 namespace CTRPluginFramework
 {
-    enum LCDSetup
-    {
-        WidthHeight = 0x5C,     ///< Framebuffer width & height  Lower 16 bits: width, upper 16 bits: height
-        FramebufferA1 = 0x68,   ///< Framebuffer A first address For top screen, this is the left eye 3D framebuffer.
-        FramebufferA2 = 0x6C,   ///< Framebuffer A second address    For top screen, this is the left eye 3D framebuffer.
-        Format = 0x70,          ///< Framebuffer format  Bit0-15: framebuffer format, bit16-31: unknown
-        Select = 0x78,          ///< Framebuffer select  Bit0: which framebuffer to display, bit1-7: unknown
-        Stride = 0x90,          ///< Framebuffer stride  Distance in bytes between the start of two framebuffer rows (must be a multiple of 8).
-        FramebufferB1 = 0x94,   ///< Framebuffer B first address For top screen, this is the right eye 3D framebuffer. Unused for bottom screen.
-        FramebufferB2 = 0x98    ///< Framebuffer B second address    For top screen, this is the right eye 3D framebuffer. Unused for bottom screen.
-    };
-
     class Screen
     {
     public:
+
+        enum LCDSetup
+        {
+            WidthHeight = 0x5C,     ///< Framebuffer width & height  Lower 16 bits: width, upper 16 bits: height
+            FramebufferA1 = 0x68,   ///< Framebuffer A first address For top screen, this is the left eye 3D framebuffer.
+            FramebufferA2 = 0x6C,   ///< Framebuffer A second address    For top screen, this is the left eye 3D framebuffer.
+            Format = 0x70,          ///< Framebuffer format  Bit0-15: framebuffer format, bit16-31: unknown
+            Select = 0x78,          ///< Framebuffer select  Bit0: which framebuffer to display, bit1-7: unknown
+            Stride = 0x90,          ///< Framebuffer stride  Distance in bytes between the start of two framebuffer rows (must be a multiple of 8).
+            FramebufferB1 = 0x94,   ///< Framebuffer B first address For top screen, this is the right eye 3D framebuffer. Unused for bottom screen.
+            FramebufferB2 = 0x98    ///< Framebuffer B second address    For top screen, this is the right eye 3D framebuffer. Unused for bottom screen.
+        };
 
         static  Screen *Top; 
         static  Screen *Bottom;
