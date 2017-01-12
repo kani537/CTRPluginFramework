a33e4131ebf374a12cb97c0379208a1d56c097da
Nanquitas <nath.doidi@gmail.com>
2017-01-12 12:47:44 +0100

    Import Clock from CPP3DS

---
+++ Includes/CTRPluginFramework/Clock.hpp
@@ -0,0 +1,20 @@
+#ifndef CTRPLUGINFRAMREWORK_CLOCK_HPP
+#define CTRPLUGINFRAMREWORK_CLOCK_HPP
+
+#include "CTRPluginFramework/Time.hpp"
+
+namespace CTRPluginFramework
+{
+    class Clock
+    {
+    public:
+        Clock(void);
+
+        Time    GetElapsedTime(void) const;
+        Time    Restart(void);
+    private:
+        Time    _startTime;
+    };
+}
+
+#endif
---
+++ Sources/CTRPluginFramework/Clock.cpp
@@ -0,0 +1,32 @@
+#include "CTRPluginFramework.hpp"
+#include "3DS.h"
+
+namespace CTRPluginFramework
+{
+    #define TICKS_PER_SEC 268123480
+
+    static Time GetCurrentTime(void)
+    {
+        return (Seconds(static_cast<float>(svcGetSystemTick())/TICKS_PER_SEC));
+    }
+
+    Clock::Clock(void) : _startTime(GetCurrentTime())
+    {
+
+    }
+
+    Time    Clock::GetElapsedTime(void) const
+    {
+        return (GetCurrentTime() - _startTime);
+    }
+
+    Time    Clock::Restart(void)
+    {
+        Time now = GetCurrentTime();
+
+        Time ret = now - _startTime;
+
+        _startTime = now;
+        return (ret);
+    }
+}
