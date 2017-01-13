96c45f266307e68f064a314293ad19012a311f15
Nanquitas <nath.doidi@gmail.com>
2017-01-13 16:14:28 +0100

    Clean PluginInit

--- Sources/pluginInit.cpp
+++ Sources/pluginInit.cpp
@@ -1,166 +1,78 @@
 #include "CTRPluginFramework.hpp"
 #include "3DS.h"
-#include "NTR.h"
 #include <stdlib.h>
 
-extern "C" void abort(void);
-
-
-u32     fsUserHandle;
-u32     sdmcArchive;
-
-extern "C" void    initSystem();
-
-int    main(void);
+extern "C" void     abort(void);
+extern "C" void     initSystem();
 
 void abort(void)
 {
-  for (;;) { }
+    CTRPluginFramework::Color red(255, 0, 0);
+    CTRPluginFramework::Screen::Top->Flash(red);
+    CTRPluginFramework::Screen::Bottom->Flash(red);
+    exit(-1);  
 }
-
-static  u32 threadStack[0x4000];
-Handle threadHandle;
 
 namespace CTRPluginFramework
 {
-    u32     copyRemoteMemory(Handle hDst, u32 ptrDst, Handle hSrc, u32 ptrSrc, u32 size)
-    {
-        u32     dmaConfig[20] = { 0 };
-        u32     hdma = 0;
-        u32     state;
-        u32     finishState = 0;
-        u32     i;
-        u32     result;
-        bool    firstError = true;
 
-        if ((result = svcFlushProcessDataCache(hSrc, (void *)ptrSrc, size)) != 0)
-            goto error;
-        if ((result = svcFlushProcessDataCache(hDst, (void *)ptrDst, size)) != 0)
-            goto error;
-    again:
-        if ((result = svcStartInterProcessDma(&hdma, hDst, (void *)ptrDst, hSrc, (void *)ptrSrc, size, dmaConfig)) != 0)
-            goto error;
-        state = 0;
-        if (finishState == 0)
-        {
-            while (1)
-            {
-                svcGetDmaState(&state, hdma);
-                svcSleepThread(10000);
-                result = state;
-                svcGetDmaState(&state, hdma);
-                if (result == state)
-                    break;
-            }
-            finishState = state;
-        }
-        else
-        {
-            for (i = 0; i < 1000000; i++)
-            {           
-                result = svcGetDmaState(&state, hdma);
-                if (state == finishState)
-                    break;
-                svcSleepThread(10000);
-            }
-            if (i >= 1000000)
-            {
-                svcCloseHandle(hdma);
-                goto error;
-            }
-        }
-        svcCloseHandle(hdma);
-        if ((result = svcInvalidateProcessDataCache(hDst, (void *)ptrDst, size)) != 0)
-            goto error;
-        return (0);
-    error:
-        return (-1);
-    }
+    static  u32 threadStack[0x4000];
+    Handle  threadHandle;
 
-    u32     protectRemoteMemory(Handle hProcess, u32 addr, u32 size)
-    {
-        return ((u32)svcControlProcessMemory(hProcess, addr, addr, size, 6, 7));
-    }
+    extern "C" int      LaunchMainThread(int arg);
 
-    u32     patchRemoteProcess(u32 pid, u32 addr, u8 *buf, u32 len)
-    {
-        u32     hProcess;
-        s32     ret;
-
-        if (R_FAILED(ret = svcOpenProcess(&hProcess, pid))) goto error;
-        if (R_FAILED(ret = protectRemoteMemory(hProcess, ((addr / 0x1000) * 0x1000), 0x1000))) goto error;
-        if (R_FAILED(ret = copyRemoteMemory(hProcess, addr, 0xffff8001, (u32)buf, len))) goto error;
-        if (hProcess)
-            svcCloseHandle(hProcess);
-        return (ret);
-    error:
-        return (-1);
-    }
-
-    void    PatchWireless(void)
-    {
-        u64 tid = Process::GetTitleID();
-
-        // Zelda OOT
-      /*  if (tid == 0x0004000000033500 
-            || tid == 0x0004000000033600
-            || tid == 0x0004000000033400)
-        {
-            //Handle  proc = 0;
-            u16     patch = 0x4770;
-            patchRemoteProcess(0x1A, 0x0105AE4, (u8 *)&patch, 2);
-        }*/
-        if (tid == 0x0004000000175E00 
-            || tid == 0x0004000000164800)
-        {
-            //Sleep(Seconds(5));
-            u32     patch  = 0xE3A01000;
-            patchRemoteProcess(Process::GetProcessID(), 0x003DFFD0, (u8 *)&patch, 4);
-            //*(vu32 *)(0x003DFFD0) = 0xE3A01000;
-        }
-    }
+    void    PatchProcess(void);
+    int     main(void);
 
     void    Initialize(void)
     {
         // Init heap and services   
         initSystem();
+        // Init Framework's system constants
         System::Initialize();
-        Process::Initialize(true);
+        // Init Process info
+        Process::Initialize(threadHandle, true);
+        // Init Screen
         Screen::Initialize();
-        // Patch games which deconnects the wireless
-        PatchWireless();        
+        // Patch process before it starts
+        PatchProcess();        
     }
-}
 
-using namespace CTRPluginFramework;
+    // Declared in ctrulib/hid
+    extern "C" vu32* hidSharedMem;
 
-void  ThreadInit(u32 arg)
-{
+    void  ThreadInit(u32 arg)
+    {
 
-    CTRPluginFramework::Initialize();
-    // Reduce Priority
-    svcSetThreadPriority(threadHandle, 0x3F);
-    // Wait for the game to be launched
-    Sleep(Seconds(5));
+        CTRPluginFramework::Initialize();
 
-    PageInfo pinfo;
-    MemInfo minfo;
+        PageInfo pinfo;
+        MemInfo minfo;
 
-    // Protect VRAM
-    svcQueryProcessMemory(&minfo, &pinfo, Process::GetHandle(), 0x1F000000);
-    svcControlProcessMemory(Process::GetHandle(), minfo.base_addr, minfo.base_addr, minfo.size, 6, 7);
+        // Protect VRAM
+        Process::ProtectRegion(0x1F000000);
+        // Reduce Priority
+        Process::Play();
+        // Wait for the game to be launched
+        Sleep(Seconds(5));
+        // Protect HID Shared Memory in case we want to push / redirects inputs
+        Process::ProtectMemory((u32)hidSharedMem, 0x1000);
 
-    int ret = main();
+        // Start plugin
+        int ret = main();
 
-    // Free heap and services and exit thread
-    exit(ret);
-}
-extern "C" int   LaunchMainThread(int arg);
+        // Release process in case it was forgotten
+        Process::Play();
 
-int   LaunchMainThread(int arg)
-{
-    rtReleaseLock(&((NS_CONFIG*)(NS_CONFIGURE_ADDR))->debugBufferLock);
-     
-    svcCreateThread(&threadHandle, ThreadInit, 0, &threadStack[0x4000], 0x18, -2);
-    return (0);
+        // Free heap and deinit services and exit thread
+        exit(ret);
+    }
+
+
+    int   LaunchMainThread(int arg)
+    {
+        svcCreateThread(&threadHandle, ThreadInit, 0, &threadStack[0x4000], 0x18, -2);
+        return (0);
+    }
+
 }
