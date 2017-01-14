06765550e1331fc6ca7aeaa5df8c346613525426
Nanquitas <nath.doidi@gmail.com>
2017-01-14 02:55:32 +0100

    Try to solve the static init issue

--- Sources/CTRPluginFramework/Color.cpp
+++ Sources/CTRPluginFramework/Color.cpp
@@ -2,15 +2,15 @@
 
 namespace CTRPluginFramework
 {
-    const Color Color::Black(0, 0, 0);
-    const Color Color::White(255, 255, 255);
-    const Color Color::Red(255, 0, 0);
-    const Color Color::Green(0, 255, 0);
-    const Color Color::Blue(0, 0, 255);
-    const Color Color::Yellow(255, 255, 0);
-    const Color Color::Magenta(255, 0, 255);
-    const Color Color::Cyan(0, 255, 255);
-    const Color Color::Transparent(0, 0, 0, 0);
+    const Color Color::Black = Color(0, 0, 0);
+    const Color Color::White = Color(255, 255, 255);
+    const Color Color::Red = Color(255, 0, 0);
+    const Color Color::Green = Color(0, 255, 0);
+    const Color Color::Blue = Color(0, 0, 255);
+    const Color Color::Yellow = Color(255, 255, 0);
+    const Color Color::Magenta = Color(255, 0, 255);
+    const Color Color::Cyan = Color(0, 255, 255);
+    const Color Color::Transparent = Color(0, 0, 0, 0);
 
     Color::Color() : r(0), g(0), b(0), a(255)
     {
