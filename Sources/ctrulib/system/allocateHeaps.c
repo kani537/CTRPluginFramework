3a1b74627f31737f25d2b32a1cee7a2151d92f9f
Nanquitas <nath.doidi@gmail.com>
2017-01-15 11:54:57 +0100

    Temporary raised the heap size

--- Sources/ctrulib/system/allocateHeaps.c
+++ Sources/ctrulib/system/allocateHeaps.c
@@ -9,16 +9,22 @@ u32 __ctru_linear_heap;
 u32 __ctru_linear_heap_size;
 //static char heap[0x10000];
 
-void __attribute__((weak)) __system_allocateHeaps(void) {
+void __attribute__((weak)) __system_allocateHeaps(void) 
+{
 	u32 tmp=0;
 
 	
-	__ctru_heap_size = 0x10000;
+	__ctru_heap_size = 0x100000;
 		//__ctru_linear_heap_size = size;
 
 	// Allocate the application heap
-	__ctru_heap = 0x07000000;
-	svcControlMemory(&tmp, __ctru_heap, 0x0, __ctru_heap_size, MEMOP_ALLOC, MEMPERM_READ | MEMPERM_WRITE | MEMPERM_EXECUTE);
+	__ctru_heap = 0x07500000;
+	if (R_FAILED(svcControlMemory(&tmp, __ctru_heap, 0x0, __ctru_heap_size, MEMOP_ALLOC, MEMPERM_READ | MEMPERM_WRITE | MEMPERM_EXECUTE)))
+	{
+		__ctru_heap_size = 0x10000;
+		svcControlMemory(&tmp, __ctru_heap, 0x0, __ctru_heap_size, MEMOP_ALLOC, MEMPERM_READ | MEMPERM_WRITE | MEMPERM_EXECUTE);
+
+	}
 
 	// Allocate the linear heap
 	//svcControlMemory(&__ctru_linear_heap, 0x0, 0x0, __ctru_linear_heap_size, MEMOP_ALLOC_LINEAR, MEMPERM_READ | MEMPERM_WRITE);
