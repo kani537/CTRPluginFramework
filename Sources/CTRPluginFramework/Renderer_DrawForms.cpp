e5329d4270e0c4366a3e6f666408ef4c494345bf
Nanquitas <nath.doidi@gmail.com>
2017-01-17 20:14:34 +0100

    Create Renderer_DrawForms

---
+++ Sources/CTRPluginFramework/Renderer_DrawForms.cpp
@@ -0,0 +1,34 @@
+#include "CTRPluginFramework.hpp"
+
+namespace CTRPluginFramework
+{
+    void        Renderer::DrawLine(int posX, int posY, int width, Color color, int height)
+    {  
+        // Correct posY
+        posY += (_rowSize[_target] - 240);
+        for (int x = 0; x < width; x++)
+        {
+            _length = height;
+            _DrawPixel(posX + x, posY + height, color);
+        }
+    }
+
+    void        Renderer::DrawRect(int posX, int posY, int width, int height, Color color, bool fill, int thickness)
+    {
+        if (fill)
+        {
+            DrawLine(posX, posY, width, color, height);
+        }
+        else
+        {
+            // Top line
+            DrawLine(posX, posY, width, color, thickness);
+            // Bottom line
+            DrawLine(posX, posY + height - (thickness - 1), width, color, thickness);
+            // Left line
+            DrawLine(posX, posY, thickness, color, height);
+            // Right line
+            DrawLine(posX + width - (thickness - 1), posY, thickness, color, height);
+        }
+    }
+}
