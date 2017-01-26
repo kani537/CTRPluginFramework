5f08f5d4fa8d0467b5a64a824ad41095f77390c5
Nanquitas <nath.doidi@gmail.com>
2017-01-26 02:07:38 +0100

    New Button Class

---
+++ Includes/CTRPluginFramework/Button.hpp
@@ -0,0 +1,102 @@
+#ifndef CTRPLUGINFRAMEWORK_BUTTON_H
+#define CTRPLUGINFRAMEWORK_BUTTON_H
+
+#include "CTRPluginFramework.hpp"
+#include "Renderer.hpp"
+#include "Touch.hpp"
+//#include "Vector.h"
+#include "Rect.hpp"
+#include <string>
+
+namespace CTRPluginFramework
+{
+    template <class C, class T, class... Args>
+    class Button
+    {
+        
+    public:
+        typedef T(C::*EventCallback)(Args...);
+        Button(std::string content, C &caller, T(C::*callback)(Args...), IntRect rect) :
+        _caller(caller), _callback(callback), _content(content), _uiProperty(rect)
+        {
+            // Black
+            borderColor = Color(1, 1, 1);
+            // Blank
+            idleColor   = Color(255, 255, 255);
+            // Dim Grey
+            pressedColor = Color(105, 105, 105);
+            // Black
+            contentColor = Color(0,255, 0);
+
+            _textSize = Renderer::GetTextSize(content.c_str());
+        }
+        ~Button(){};
+
+        virtual bool    operator ()(Args&... args)
+        {
+            static bool isReady = true;
+
+            // Check if is pressed
+            bool isPressed = _Update();
+            // Draw button
+            _Draw(isPressed);
+
+            // If is ready, then execute the function
+            if (isPressed && isReady)
+            {
+                if (_callback)
+                    (_caller.*_callback)(args...);
+                isReady = false;
+            }
+            if (!isPressed && !isReady)
+            {
+                isReady = true;
+            }
+        }
+
+        Color           borderColor;
+        Color           idleColor;
+        Color           pressedColor;
+        Color           contentColor;
+    private:
+
+        virtual bool    _Update(void)
+        {
+            if (!Touch::IsDown())
+                return (false);
+
+            IntVector touch = IntVector(Touch::GetPosition());
+            return (_uiProperty.Contains(touch));
+        }
+
+        virtual void    _Draw(bool isPressed)
+        {
+            Renderer::RoundedRectangle(_uiProperty, 7, 50, borderColor, true, isPressed ? pressedColor : idleColor);
+            
+            int posX = _uiProperty._leftTopCorner.x;
+            int posY = _uiProperty._leftTopCorner.y;
+
+            int height = _uiProperty._size.y;
+            int width = _uiProperty._size.x;
+            int limit = posX + width;
+
+            posY += (height - 16) / 2;
+            int x = (width - _textSize) / 2;
+            if (x > 0)
+                posX += x;
+
+            Renderer::DrawSysString(_content.c_str(), posX, posY, limit, contentColor);        
+        }
+
+
+        C               &_caller;
+        EventCallback   _callback;
+        std::string     _content;
+        IntRect         _uiProperty;
+        float           _textSize;
+
+    };
+
+}
+
+#endif
