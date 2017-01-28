451901fd91710df7c4d239201f0651d848b10750
Nanquitas <nath.doidi@gmail.com>
2017-01-28 17:39:08 +0100

    Update Button template

--- Includes/CTRPluginFramework/Graphics/Button.hpp
+++ Includes/CTRPluginFramework/Graphics/Button.hpp
@@ -13,15 +13,23 @@
 
 namespace CTRPluginFramework
 {
+    // class C -> type of the caller object
+    // class T -> return type of the callback
+    // Args -> potential args for the callback
     template <class C, class T, class... Args>
     class Button
     {
         
     public:
         using EventCallback = T (C::*)(Args...);
-        //typedef T(C::*EventCallback)(Args...);
-        Button(std::string content, C &caller, T(C::*callback)(Args...), IntRect rect) :
-        _caller(caller), _callback(callback), _content(content), _uiProperty(rect)
+        using IconCallback = int (*)(int, int);
+
+        Button(std::string content, C &caller, T(C::*callback)(Args...), IntRect rect, IconCallback icon = nullptr) :
+        _caller(caller), 
+        _callback(callback), 
+        _content(content), 
+        _uiProperty(rect),
+        _icon(icon)
         {
             // Black
             borderColor = Color(165, 165, 165);
@@ -33,113 +41,141 @@ namespace CTRPluginFramework
             contentColor = Color();
 
             _textSize = Renderer::GetTextSize(content.c_str());
+            if (icon != nullptr)
+                _textSize += 18.f;
 
             Renderer::ComputeRoundedRectangle(_lines, _uiProperty, 7, 50);
+            _isPressed = false;
+            _execute = false;
         }
         ~Button(){};
 
-        virtual bool    operator ()(bool isTouchDown, IntVector touchPos, Args&... args)
+        virtual bool    operator ()(Args&... args)
         {
-            static bool isReady = true;
-
-            // Check if is pressed
-            bool isPressed = false;
-
-            if (isTouchDown)
+            if (_execute)
             {
-                isPressed = _uiProperty.Contains(touchPos);
-            }
-
-            // Draw button
-            _Draw(isPressed);
-
-            // If is ready, then execute the function
-            if (isPressed && isReady)
-            {
-                if (_callback)
-                    (_caller.*_callback)(args...);
-                isReady = false;
+                (_caller.*(_callback))(args...);
+                _execute = false;
                 return (true);
-            }
-            if (!isPressed && !isReady)
-            {
-                isReady = true;
             }
             return (false);
         }
+
+        virtual void    Draw(void);
+        virtual void    Update(bool isTouchDown, IntVector touchPos);
 
         Color           borderColor;
         Color           idleColor;
         Color           pressedColor;
         Color           contentColor;
+        u8              icon;
     private:
 
-        virtual void    _Draw(bool isPressed)
-        {
-            //Renderer::RoundedRectangle(_uiProperty, 7, 50, borderColor, true, isPressed ? pressedColor : idleColor);
-            
-            Color &fillColor = isPressed ? pressedColor : idleColor;
-            int bMax = _lines.size() - 5;
-            int i;
-
-            for (i = 0; i < bMax; i++)
-            {
-                IntLine &line = _lines[i];
-                // Draw border
-                Renderer::_DrawPixel(line.start.x, line.start.y, borderColor);
-                Renderer::_DrawPixel(line.end.x, line.end.y, borderColor);
-               /* if (i == 0)
-                {
-                    char buf[40];
-                    sprintf(buf, "[%d, %d] - [%d, %d]", line.start.x, line.start.y, line.end.x, line.end.y);
-                    int posY = 50;
-                    Color blank(255, 255, 255);
-                    Renderer::DrawString(buf, 150, posY, blank);
-                }*/
-                // Fill line
-                IntVector left(line.start);
-                IntVector right(line.end);
-                left.x++;
-                right.x;
-                Renderer::DrawLine(left, right, fillColor);
-            }
-
-            for (; i < _lines.size() - 1; i++)
-            {
-                IntLine &line = _lines[i];
-
-                Renderer::DrawLine(line.start.x, line.start.y, line.end.x, borderColor, line.end.y);
-            }
-
-            IntLine &line = _lines[i];
-            Renderer::DrawLine(line.start.x, line.start.y, line.end.x, fillColor, line.end.y);
-
-            int posX = _uiProperty._leftTopCorner.x;
-            int posY = _uiProperty._leftTopCorner.y;
-
-            int height = _uiProperty._size.y;
-            int width = _uiProperty._size.x;
-            int limit = posX + width;
-
-            posY += (height - 16) / 2;
-            //posY += (height - 10) / 2;
-            int x = (width - _textSize) / 2;
-            if (x > 0)
-                posX += x;
-            //Renderer::DrawString((char *)_content.c_str(), posX, posY, contentColor);
-            Renderer::DrawSysString(_content.c_str(), posX, posY, limit, contentColor);        
-        }
+        
 
 
-        C               &_caller;
-        EventCallback   _callback;
-        std::string     _content;
-        IntRect         _uiProperty;
-        float           _textSize;
-        std::vector<IntLine> _lines;
+        C                       &_caller;
+        EventCallback           _callback;
+        IconCallback            _icon;
+        std::vector<IntLine>    _lines;
 
+        std::string             _content;
+        IntRect                 _uiProperty;
+        float                   _textSize;        
+        bool                    _isPressed;
+        bool                    _execute;
     };
 
+    /*
+    ** Draw
+    *********/
+
+    template <class C, class T, class... Args>
+    void    Button<C, T, Args...>::Draw(void)
+    {
+        //Renderer::RoundedRectangle(_uiProperty, 7, 50, borderColor, true, isPressed ? pressedColor : idleColor);
+        
+        Color &fillColor = _isPressed ? pressedColor : idleColor;
+        int bMax = _lines.size() - 5;
+        int i;
+
+        for (i = 0; i < bMax; i++)
+        {
+            IntLine &line = _lines[i];
+            // Draw border
+            Renderer::_DrawPixel(line.start.x, line.start.y, borderColor);
+            Renderer::_DrawPixel(line.end.x, line.end.y, borderColor);
+
+            // Fill line
+            IntVector left(line.start);
+            IntVector right(line.end);
+            left.x++;
+            right.x;
+            Renderer::DrawLine(left, right, fillColor);
+        }
+
+        for (; i < _lines.size() - 1; i++)
+        {
+            IntLine &line = _lines[i];
+
+            Renderer::DrawLine(line.start.x, line.start.y, line.end.x, borderColor, line.end.y);
+        }
+
+        IntLine &line = _lines[i];
+        Renderer::DrawLine(line.start.x, line.start.y, line.end.x, fillColor, line.end.y);
+
+        int posX = _uiProperty._leftTopCorner.x;
+        int posY = _uiProperty._leftTopCorner.y;
+
+        int height = _uiProperty._size.y;
+        int width = _uiProperty._size.x;
+        int limit = posX + width;
+
+        posY += (height - 16) / 2;
+
+        int posXI = 0;
+
+        
+        //posY += (height - 10) / 2;
+        int x = (width - _textSize) / 2;
+        if (x > 0)
+            posX += x;
+
+        if (_icon != nullptr)
+        {
+            posX = _icon(posX, posY) + 3;
+        }
+        //Renderer::DrawString((char *)_content.c_str(), posX, posY, contentColor);
+        Renderer::DrawSysString(_content.c_str(), posX, posY, limit, contentColor);        
+    }
+
+    /*
+    ** Update
+    ***********/
+    template <class C, class T, class... Args>
+    void    Button<C,T, Args...>::Update(bool isTouchDown, IntVector touchPos)
+    {
+        static bool isReady = true;
+
+        // Check if is pressed
+        _isPressed = false;
+
+        if (isTouchDown)
+        {
+            _isPressed = _uiProperty.Contains(touchPos); 
+        }
+
+        // If is ready, then execute the function
+        if (_isPressed && isReady)
+        {
+            isReady = false;
+        }
+        if (!_isPressed && !isReady)
+        {
+            isReady = true;
+            _execute = true;
+        }
+    }
 }
 
 #endif
