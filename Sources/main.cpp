b2422723e8e1220c295355d996446ddd79f2fdd5
Nanquitas <nath.doidi@gmail.com>
2017-01-30 15:55:35 +0100

    Add lorem ipsum note

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -27,11 +27,18 @@ namespace CTRPluginFramework
 
         Menu    menu("Zelda Ocarina Of Time 3D");
 
+        std::string text = 
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id semper ligula. Vivamus sollicitudin lacinia ligula, vel hendrerit massa posuere in. Aliquam eget euismod tortor, vel ultricies sem. Donec tempor odio vel neque suscipit finibus ac at quam. Cras pharetra, massa scelerisque rutrum pretium, sapien tortor lobortis elit, vel euismod dui dolor non leo. Praesent sodales sagittis purus quis feugiat. Praesent est massa, gravida et ultrices in, aliquet ac tortor. Nam id tempus dolor. Pellentesque lobortis porta sagittis. Phasellus malesuada pulvinar porttitor.\n\n" \
+"Donec ultrices tortor sit amet nibh tincidunt dapibus. Sed interdum dignissim nibh, vel convallis velit volutpat ut. Phasellus blandit, ligula ut varius luctus, quam mi maximus massa, at mattis sem ligula ac mi. Vestibulum tempor fermentum pretium. Phasellus in metus elit. Donec eget massa eu libero lacinia accumsan. Phasellus non velit eget mauris gravida ornare. Sed sodales nulla in dictum pellentesque.\n\n" \
+"Nam magna nunc, posuere ut nisi id, cursus ultricies dui. Suspendisse efficitur id dolor sit amet rhoncus. Sed tincidunt arcu nunc, quis maximus nunc maximus a. Quisque pellentesque libero quis urna rhoncus, eget rutrum diam tincidunt. Nulla eget ultricies nisl, in condimentum nulla. Nullam nec turpis a tellus faucibus dictum ut quis eros. Etiam placerat fringilla nisl at convallis. Ut sit amet ligula accumsan, feugiat neque maximus, sollicitudin nulla. Curabitur posuere, enim ut placerat tincidunt, dui tellus ullamcorper lorem, eu feugiat dui leo quis velit. Morbi non velit eget ipsum sodales sagittis in vel urna.\n\n" \
+"unc sem eros, fermentum sed justo sit amet, condimentum tempus nunc. Mauris elementum vulputate tempus. Proin iaculis justo rhoncus, lobortis massa nec, tempus dolor. Donec et dictum erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed convallis arcu et nisi posuere, vel fermentum est cursus. Phasellus hendrerit, tellus id bibendum auctor, odio elit blandit justo, sed porttitor diam lorem eu lacus. Phasellus ac nibh venenatis, sagittis enim ultrices, efficitur tellus. Vestibulum ullamcorper egestas nunc eget volutpat. In tempus eros eu ipsum feugiat placerat. Cras iaculis odio nec nunc faucibus, in blandit turpis lacinia.\n\n" \
+"Aenean a massa lacinia, elementum urna aliquam, sodales lorem. Vivamus hendrerit egestas orci vitae interdum. Aenean ultricies justo mi, et porta lacus placerat at. Sed faucibus nulla viverra dolor luctus, eget blandit sapien vestibulum. Duis commodo varius vulputate. Quisque ut dui lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quis tellus porttitor, pellentesque sem mollis, eleifend felis.\n\n";
+
         /*
         ** Movements codes
         ********************/
 
-        MenuFolder *folder = new MenuFolder("Movement");
+        MenuFolder *folder = new MenuFolder("Movement", text);
 
         folder->Append(new MenuEntry("MoonJump (\uE000)", MoonJump, "Press \uE000 to be free of the gravity."));
         folder->Append(new MenuEntry("Fast Move (\uE077 + \uE054)", MoveFast, "Use \uE077 while pressing \uE054 to move very fast. Be careful of the loading zone, it might put you out of bound."));
