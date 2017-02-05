24efedb3931a78eac0a59b4613f80ddc097b7e20
Nanquitas <nath.doidi@gmail.com>
2017-02-05 16:11:05 +0100

    Clean Controller

--- Sources/CTRPluginFramework/Controller.cpp
+++ Sources/CTRPluginFramework/Controller.cpp
@@ -1,6 +1,6 @@
 #include "types.h"
 #include "ctrulib/services/hid.h"
-
+#include "ctrulib/svc.h"
 #include "CTRPluginFramework/Controller.hpp"
 
 namespace CTRPluginFramework
@@ -44,21 +44,22 @@ namespace CTRPluginFramework
             return (true);
         return (false);
     }
-    extern "C" u32 hidCheckSectionUpdateTime(vu32 *sharedmem_section, u32 id);
 
+    // TODO: a better injection, touch input injection being not good enough
     void    Controller::InjectTouch(u16 posX, u16 posY)
     {
-        //u32 id = hidSharedMem[42 + 4];
         touchPosition tpos = {.px = posX, .py = posY};
 
-       // if (id > 7) id = 7;
-        //if (hidCheckSectionUpdateTime(&hidSharedMem[42], id)==0)
         for (int i = 0; i < 8; i++)
         {
-            int j = 42 + 8 + i *2;
+            int j = 42 + 8 + i * 2;
             *(touchPosition *)(&hidSharedMem[j]) = tpos;
             hidSharedMem[j + 1] = 1;           
         }
+
+        u64 tick = svcGetSystemTick();
+        hidSharedMem[42] = 0;
+        *((u64*)&hidSharedMem[42]) = tick;
     }
 
     void    Controller::InjectKey(u32 key)
@@ -68,17 +69,6 @@ namespace CTRPluginFramework
             int j = 10 + i * 4;
             *(u32 *)(&hidSharedMem[j]) |= key;        
         }       
-    }
-
-    extern "C"     void    CtruInjectTouch(vu32 *hid);
-    bool inject = false;
-    void    CtruInjectTouch(vu32 *hid)
-    {
-            touchPosition tpos = {0};
-            tpos.px = 10;
-            tpos.py = 10; 
-            *(touchPosition *)hid = tpos;
-            *(hid + 1) = 1;      
     }
 
     void    Controller::Update(void)
