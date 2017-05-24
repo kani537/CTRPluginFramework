f95b039e7582532e41833347844e89f6ec1f0b60
Nanquitas <nath.doidi@gmail.com>
2017-05-24 12:20:41 +0200

    allocateHeaps: try in EcoMemoryMode in case of failure

--- Sources/ctrulib/system/allocateHeaps.cpp
+++ Sources/ctrulib/system/allocateHeaps.cpp
@@ -2,6 +2,7 @@
 #include "CTRPluginFramework.hpp"
 #include "CTRPluginFrameworkImpl/arm11kCommands.h"
 #include "CTRPluginFrameworkImpl/System/Screen.hpp"
+#include "CTRPluginFrameworkImpl/Preferences.hpp"
 
 namespace CTRPluginFramework
 {
@@ -36,8 +37,23 @@ namespace CTRPluginFramework
         __ctru_linear_heap_size = 0x150000;
         if (R_FAILED(arm11kSvcControlMemory(&__ctru_linear_heap, 0, __ctru_linear_heap_size, g_linearOp, MEMPERM_READ | MEMPERM_WRITE | MEMPERM_EXECUTE)))
         {
-            Screen::Bottom->Flash(red);
-            g_heapError = true;
+            // Try again with a different mode
+            if (R_FAILED(arm11kSvcControlMemory(&__ctru_linear_heap, 0, __ctru_linear_heap_size, 0x10203u, MEMPERM_READ | MEMPERM_WRITE | MEMPERM_EXECUTE)))
+            {
+                // Try again in EcoMode
+                Preferences::EcoMemoryMode = true;
+
+                __ctru_linear_heap_size = 0x50000;
+
+                if (R_FAILED(arm11kSvcControlMemory(&__ctru_linear_heap, 0, __ctru_linear_heap_size, g_linearOp, MEMPERM_READ | MEMPERM_WRITE | MEMPERM_EXECUTE)))
+                {
+                    if (R_FAILED(arm11kSvcControlMemory(&__ctru_linear_heap, 0, __ctru_linear_heap_size, 0x10203u, MEMPERM_READ | MEMPERM_WRITE | MEMPERM_EXECUTE)))
+                    {
+                        Screen::Bottom->Flash(red);
+                        g_heapError = true;
+                    }
+                }
+            }           
         }
 
         // Set up newlib heap
