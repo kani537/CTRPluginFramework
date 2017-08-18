b0ab76d03967206caaf39c6123c8ebd78f348a3f
Nanquitas <nath.doidi@gmail.com>
2017-08-18 12:08:50 +0200

    Update Utils

--- Includes/CTRPluginFramework/Utils/Utils.hpp
+++ Includes/CTRPluginFramework/Utils/Utils.hpp
@@ -10,9 +10,41 @@ namespace CTRPluginFramework
     {
     public:
 
+        /**
+         * \brief Get a string formatted with format specifier from printf
+         * \param fmt String to be formatted
+         * \param ... Addtionnal arguments
+         * \return The formatted std::string
+         */
         static std::string  Format(const char *fmt, ...);
+
+        /**
+         * \brief Get a random number
+         * \return A random number
+         */
         static u32          Random(void);
+
+        /**
+         * \brief Get a random number
+         * \param min Minimu value for the random number
+         * \param max Maximum value for the random number
+         * \return A random number between min & max
+         */
         static u32          Random(u32 min, u32 max);
+
+        /**
+         * \brief Get the size of an utf8 std::string (max size 0x100)
+         * \param str The string to count
+         * \return The count of utf8 chars in the str
+         */
+        static u32          GetSize(const std::string &str);
+
+        /**
+         * \brief Remove the last char of an utf8 string (max size 0x100)
+         * \param str The string to remove the char from
+         * \return The codepoint value of the char removed
+         */
+        static u32          RemoveLastChar(std::string &str);
     };
 }
 
--- Sources/CTRPluginFramework/Utils/Utils.cpp
+++ Sources/CTRPluginFramework/Utils/Utils.cpp
@@ -1,7 +1,9 @@
 #include "CTRPluginFramework/Utils/Utils.hpp"
 #include <cstdarg>
 #include <cstdio>
+#include <cstring>
 #include <random>
+#include "ctrulib/util/utf.h"
 
 namespace CTRPluginFramework
 {
@@ -37,5 +39,59 @@ namespace CTRPluginFramework
         std::uniform_int_distribution<u32>  uniform(min, max);
 
         return (uniform(g_rng));
+    }
+
+    u32     Utils::GetSize(const std::string &str)
+    {
+        u32     size = str.length();
+        u8      buffer[0x100] = { 0 };
+        u8      *s = buffer;
+
+        if (!size) return (0);
+
+        std::memcpy(buffer, str.data(), size);
+
+        size = 0;
+        while (*s)
+        {
+            u32 code;
+            int units = decode_utf8(&code, s);
+
+            if (units == -1)
+                break;
+
+            s += units;
+            size++;
+        }
+        return (size);
+    }
+
+    u32     Utils::RemoveLastChar(std::string &str)
+    {
+        u32     size = str.length();
+        u8      buffer[0x100] = { 0 };
+        u8      *s = buffer;
+
+        if (!size) return (0);
+
+        std::memcpy(buffer, str.data(), size);
+
+        while (*s)
+        {
+            u32 code;
+            int units = decode_utf8(&code, s);
+
+            if (units == -1)
+                break;
+            if (*(s + units))
+                s += units;
+            else
+            {
+                *s = 0;
+                str = reinterpret_cast<char *>(buffer);
+                return (code);
+            }
+        }
+        return (0);
     }
 }
