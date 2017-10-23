be987009ab2125d109ec26bfe76f082ee466af98
Nanquitas <nath.doidi@gmail.com>
2017-10-23 12:39:12 +0200

    Beta 0.3.0

--- Sources/CTRPluginFrameworkImpl/Menu/PluginMenuTools.cpp
+++ Sources/CTRPluginFrameworkImpl/Menu/PluginMenuTools.cpp
@@ -515,7 +515,7 @@ namespace CTRPluginFramework
 
         // Draw Framework version
         {
-            static const char *version = "CTRPluginFramework Alpha V.0.2.11";
+            static const char *version = "CTRPluginFramework Beta V.0.3.0";
             static const u32 xpos = (320 - Renderer::LinuxFontSize(version)) / 2;
 
             int posY = 205;
