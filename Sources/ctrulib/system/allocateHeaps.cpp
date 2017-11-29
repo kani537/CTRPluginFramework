1b9782910101e562f048b5d270f6ef44a65c8076
Nanquitas <nath.doidi@gmail.com>
2017-11-29 09:07:56 +0100

    Use NTR region as a heap

--- Sources/ctrulib/system/allocateHeaps.cpp
+++ Sources/ctrulib/system/allocateHeaps.cpp
@@ -15,26 +15,63 @@ namespace CTRPluginFramework
     extern "C" u32 __ctru_linear_heap_size;
 
     bool    g_heapError = false;
-    u32	    g_linearOp = 0x10003u;
+    //u32	    g_linearOp = 0x10003u;
 
     extern "C" void             __system_allocateHeaps(void);
     void __attribute__((weak))  __system_allocateHeaps(void)
     {
         Color red(255, 0, 0);
 
-        // Allocate the main heap
-        __ctru_heap = 0x07500000;
-        __ctru_heap_size = 0x100000;
+        u32     NTRSize = 0;
+        bool    isLoaderNTR = Process::CheckRegion(0x06000000, NTRSize, 7);
 
-        if (R_FAILED(arm11kSvcControlMemory(&__ctru_heap, __ctru_heap, __ctru_heap_size, 0x203u, MEMPERM_READ | MEMPERM_WRITE | MEMPERM_EXECUTE)))
+        if (isLoaderNTR)
         {
-            ScreenImpl::Top->Flash(red);
-            g_heapError = true;
-            return;
+            // Heap params
+            __ctru_heap = 0x06000100; ///< Leave enough space to relocate (nop ?) NTR's shared functions
+            __ctru_heap_size = 0x100000; // O3DS: reduce heap size for Mode 3 ?
+
+            u32 temp = 0x06000000 + NTRSize;
+            u32 *heap = (u32 *)0x06000000;
+
+            // TODO: check for Mode3 or O3DS special games to avoid allocating too much memory
+            if (R_SUCCEEDED(arm11kSvcControlMemory(&temp, temp, __ctru_heap_size, 0x203, 3)))
+            {
+                // Fix heap perms
+                Process::ProtectRegion(temp, 7);
+                Process::CheckRegion(0x06000000, __ctru_heap_size, 7);
+
+                // Clear memory
+                for (u32 i = 0; i < __ctru_heap_size / 4; i++)
+                    *heap++ = 0;
+            }
+        }
+        else
+        {
+            // Heap params
+            __ctru_heap = 0x06000000;
+            __ctru_heap_size = 0x200000; // O3DS: reduce heap size for Mode 3 ?
+
+            while (R_FAILED(arm11kSvcControlMemory(&__ctru_heap, __ctru_heap, __ctru_heap_size, 0x203u, MEMPERM_READ | MEMPERM_WRITE)))
+            {
+                __ctru_heap_size -= 0x1000;
+            }
+
+            // Fix heap perms
+            if (!Process::ProtectRegion(__ctru_heap, 7))
+                goto fatal;
         }
 
+        // Set up newlib heap
+        fake_heap_start = reinterpret_cast<char*>(__ctru_heap);
+        fake_heap_end = fake_heap_start + __ctru_heap_size;
+        return;
+    fatal:
+        ScreenImpl::Top->Flash(red);
+        g_heapError = true;
+
         // Allocate the linear heap
-        __ctru_linear_heap_size = 0x100000;
+        /*__ctru_linear_heap_size = 0x100000;
         if (R_FAILED(arm11kSvcControlMemory(&__ctru_linear_heap, 0, __ctru_linear_heap_size, g_linearOp, MEMPERM_READ | MEMPERM_WRITE | MEMPERM_EXECUTE)))
         {
             // Try again with a different mode
@@ -54,10 +91,6 @@ namespace CTRPluginFramework
                     }
                 }
             }           
-        }
-
-        // Set up newlib heap
-        fake_heap_start = reinterpret_cast<char*>(__ctru_heap);
-        fake_heap_end = fake_heap_start + __ctru_heap_size;
+        }*/
     }
 }
