61201f232a10504e32040bef49f830e8a0ef3769
Nanquitas <nath.doidi@gmail.com>
2018-04-24 00:24:55 +0200

    Alpha 0.4.4

--- Sources/CTRPluginFrameworkImpl/Menu/PluginMenuTools.cpp
+++ Sources/CTRPluginFrameworkImpl/Menu/PluginMenuTools.cpp
@@ -26,7 +26,7 @@
 #define ALPHA 1
 
 #if ALPHA
-#define VersionStr "CTRPluginFramework Alpha V.0.4.3"
+#define VersionStr "CTRPluginFramework Alpha V.0.4.4"
 #else
 #define VersionStr "CTRPluginFramework Beta V.0.4.0"
 #endif
