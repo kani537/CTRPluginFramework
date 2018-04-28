4543d1d4a20c75a81c53dc631c53b6591f1e310d
Nanquitas <nath.doidi@gmail.com>
2018-04-28 13:02:12 +0200

    minor cleaning

--- Sources/ctrulib/system/allocateHeaps.cpp
+++ Sources/ctrulib/system/allocateHeaps.cpp
@@ -1,21 +1,17 @@
 #include <3DS.h>
 #include "CTRPluginFramework.hpp"
-#include "CTRPluginFrameworkImpl/arm11kCommands.h"
 #include "CTRPluginFrameworkImpl/System/Screen.hpp"
-#include "CTRPluginFrameworkImpl/Preferences.hpp"
+#include "csvc.h"
 
 namespace CTRPluginFramework
 {
     extern "C" char *fake_heap_start;
     extern "C" char *fake_heap_end;
-    extern "C" u32 __tmp;
+
     extern "C" u32 __ctru_heap;
     extern "C" u32 __ctru_heap_size;
-    extern "C" u32 __ctru_linear_heap;
-    extern "C" u32 __ctru_linear_heap_size;
 
     bool    g_heapError = false;
-    //u32	    g_linearOp = 0x10003u;
 
     extern "C" void             __system_allocateHeaps(void);
     void __system_allocateHeaps(void)
@@ -32,7 +28,7 @@ namespace CTRPluginFramework
             __ctru_heap = 0x07500000;
 
             // TODO: check for Mode3 or O3DS special games to avoid allocating too much memory
-            if (R_SUCCEEDED(arm11kSvcControlMemory(&__ctru_heap, __ctru_heap, __ctru_heap_size, 0x203, 3)))
+            if (R_SUCCEEDED(svcControlMemoryEx(&__ctru_heap, __ctru_heap, __ctru_heap, __ctru_heap_size, (MemOp)0x203, (MemPerm)3, true)))
             {
                 // Fix heap perms
                 Process::CheckRegion(__ctru_heap, __ctru_heap_size, 7);
@@ -45,8 +41,11 @@ namespace CTRPluginFramework
             // Heap params
             __ctru_heap = 0x06000000;
 
-            while (R_FAILED(arm11kSvcControlMemory(&__ctru_heap, __ctru_heap, __ctru_heap_size, 0x203u, MEMPERM_READ | MEMPERM_WRITE)))
+            while (R_FAILED(svcControlMemoryEx(&__ctru_heap, __ctru_heap, __ctru_heap, __ctru_heap_size, (MemOp)0x203u, (MemPerm)(MEMPERM_READ | MEMPERM_WRITE), true)))
             {
+                if (__ctru_heap_size <= 0x100000)
+                    goto fatal;
+
                 __ctru_heap_size -= 0x1000;
             }
 
