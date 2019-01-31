394210b1cecce62d1e99cec701a895646e7562f5
Nanquitas <nath.doidi@gmail.com>
2019-01-31 16:01:59 +0100

    PluginMenuSearch: fix JumpToHexEditor``

--- Sources/CTRPluginFrameworkImpl/Menu/PluginMenuSearch.cpp
+++ Sources/CTRPluginFrameworkImpl/Menu/PluginMenuSearch.cpp
@@ -81,6 +81,7 @@ namespace CTRPluginFramework
         {
             if (_hexEditor(eventList))
                 _inEditor = false;
+            Renderer::EndFrame();
             return (false);
         }
 
