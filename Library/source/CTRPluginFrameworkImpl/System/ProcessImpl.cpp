d4f7adae12aace0747b68cc7a1fbeb5d98ff7d09
PabloMK7 <hackyglitch@gmail.com>
2020-07-25 17:23:03 +0200

    GSP: ensure sharedmem / fb is accessible

--- Library/source/CTRPluginFrameworkImpl/System/ProcessImpl.cpp
+++ Library/source/CTRPluginFrameworkImpl/System/ProcessImpl.cpp
@@ -389,10 +389,9 @@ namespace CTRPluginFramework
         // Resume interrupt reciever and acquire screens
         // NOTE: NEEDS TO BE DISABLED IF THIS FUNCTION IS MADE TO RETURN EXECUTION
         GSP::ResumeInterruptReceiver();
-        ScreenImpl::AcquireFromGsp();
-
-        // Update OSD screens
-        OSDImpl::UpdateScreens();
+        if (!ScreenImpl::AcquireFromGsp())
+            // Update OSD screens
+            OSDImpl::UpdateScreens();
 
         // Update memregions, this layout is used by internal checks
         UpdateMemRegions(true);
